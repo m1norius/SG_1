03f4f5310ee8ce5fda919b017fba985a47e8c858
minorius <minorius.wts@gmail.com>
2017-01-18 00:19:40 +0200

    1

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/**
+ * Created by minorius on 17.01.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("111");
+
+    }
+}
